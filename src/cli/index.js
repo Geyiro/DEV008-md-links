dbd1c4bc26f1bfff54befb81d0971c6bd06240e1
Erika Nagaki <enagaki.22@gmail.com>
2023-09-04 12:18:05 -0500

    welcome and instruction message

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,4 +1,3 @@
-import fs, { link } from "node:fs";
 import chalk from "chalk";
 import { API } from "../api/index.js";
 import { mdLinks } from "../api/mdlinks.js";
@@ -11,7 +10,37 @@ export const CLI = {
     const paths = args.filter((arg) => !options.includes(arg));
 
     if (paths.length < 1) {
-      throw new Error(chalk.red("Path is required"));
+      console.log(
+        chalk.blue.bold(`
+      
+                        ╔═════════════════════════════════════════╗
+                        ║      Lets get started with Md-Links     ║
+                        ╚═════════════════════════════════════════╝
+      `)
+      );
+      console.log(
+        chalk.blueBright(
+          `
+      ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+      ┃                                                                                ┃
+      ┃           First you gotta input the path thats going to be analized            ┃
+      ┃                                                                                ┃
+      ┃                      ` +
+            "➥ Md-Links" +
+            "   " +
+            chalk.bgBlue.bold("./path") +
+            "   " +
+            chalk.bgCyan.bold("--option") +
+            `                            ┃
+      ┃                                                                                ┃
+      ┃     for options:                                                               ┃
+      ┃  --validate: If you want to check each individual link status                  ┃
+      ┃  --stats: Gives an overall statistic of the links                              ┃
+      ┃  --stats --validate: Returns how many broken links are + general statistics    ┃
+      ┃                                                                                ┃
+      ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛`
+        )
+      );
     } else if (paths.length > 1) {
       throw new Error(chalk.red("Multiple paths present, only one needed"));
     }
@@ -34,21 +63,28 @@ export const CLI = {
         });
       });
     } else if (stats && !validate) {
-      mdLinks(args.path).then((links) => {
-        console.log(`\n${"Total :"} ${links.length}`);
-        console.log(`${"Unique :"} ${API.uniqueStats(links)}`);
-        // .catch((error) => console.error(error));
-      });
+      mdLinks(args.path)
+        .then((links) => {
+          console.log(chalk.blue.bold(`\n${"TOTAL :"}`) + ` ${links.length}`);
+          console.log(
+            chalk.blue.bold(`${"UNIQUE :"}`) + ` ${API.uniqueStats(links)}`
+          );
+        })
+        .catch((error) => console.error(error));
     } else if ((stats && validate) || (validate && stats)) {
       mdLinks(args.path).then((links) => {
-        console.log(`\n${"Total :"} ${links.length}`);
-        console.log(`${"Unique :"} ${API.uniqueStats(links)}`);
-
-        API.brokenLinks(links).then((brokenCount) =>
-          console.log(`${"Broken :"} ${brokenCount}`)
+        console.log(chalk.blue.bold(`\n${"TOTAL :"}`) + ` ${links.length}`);
+        console.log(
+          chalk.blue.bold(`${"UNIQUE :"}`) + ` ${API.uniqueStats(links)}`
         );
 
-        // .catch((error) => console.error(error));
+        API.brokenLinks(links)
+          .then((brokenCount) =>
+            console.log(
+              chalk.red.bold(`${"BROKEN :"}`) + chalk.red(` ${brokenCount}`)
+            )
+          )
+          .catch((error) => console.error(error));
       });
     }
     // });
